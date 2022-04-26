01e5dfe43b54c39f517f6e407f9018a217b3dc0b
Anya Miloserdnaya <milos3rdnaya@yandex.ru>
2022-04-26 14:43:47 +0300

    conditional show input

--- src/components/to-do-item/ToDoItem.js
+++ src/components/to-do-item/ToDoItem.js
@@ -2,10 +2,9 @@ import './to-do-item.scss';
 import { deleteToDo, editToDo, toggleComplete } from '../../redux/toDoSlice';
 import { useDispatch } from 'react-redux';
 
-const isEditing = false;
-
 const ToDoItem = ({id, text, completed}) => {
   const dispatch = useDispatch();
+  let isEditing = false;
 
   const onDeleteToDo = () => {
     dispatch(
@@ -28,19 +27,24 @@ const ToDoItem = ({id, text, completed}) => {
     }
   };
 
+  const toggleEditing = () => {
+    isEditing = !isEditing;
+  }
+
   return (
-    <li className={`to-do-item ${completed ? 'to-do-item--completed' :''} ${isEditing ? 'to-do-item--editing' :''}`}>
+    <li className={`to-do-item ${completed ? 'to-do-item--completed':''} ${isEditing ? 'to-do-item--editing':''}`}>
       <button onClick={onToggleComplete} className="to-do-item__toggle"></button>
-      <span onDoubleClick={onEditToDo} className="to-do-item__text">
+      { !isEditing && <span onDoubleClick={toggleEditing} className="to-do-item__text"> 
         {text}
-      </span>
+      </span> }
+      { isEditing &&
       <input 
         type="text"
         className="to-do-item__edit"
         value={text}
         onChange={onEditToDo}
       >
-      </input>
+      </input> }
       <button onClick={onDeleteToDo} className="to-do-item__del"></button>
     </li>
   );
--- src/redux/toDoSlice.js
+++ src/redux/toDoSlice.js
@@ -21,7 +21,7 @@ export const toDoSlice = createSlice({
     },
     editToDo: (state, action) => {
       let index = state.findIndex(toDo => toDo.id === action.payload.id);
-      // state[index].text = action.payload.newText;
+      state[index].text = action.payload.newText;
     }
   }
 });
