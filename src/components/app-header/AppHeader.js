32c752780c3ac07bf4c6cc1ea3ed683c04a85567
Anya Miloserdnaya <milos3rdnaya@yandex.ru>
2022-04-28 17:08:24 +0300

    hints

--- src/components/app-header/AppHeader.js
+++ src/components/app-header/AppHeader.js
@@ -4,6 +4,8 @@ import './app-header.scss';
 const AppHeader = () => {
 	return (
 		<header className="app-header">
+			<p className="hint hint--left">You can add/delete/complete your awesome todos!</p>
+			<p className="hint hint--right">Edit on double click, save on enter</p>
 			<img src={logo} className="app-header__logo" alt="logo" />
 			<h1 className="app-header__heading">Todos</h1>
 		</header>
